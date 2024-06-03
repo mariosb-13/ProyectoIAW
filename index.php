11944d18a0dfe461b980a92953085309d962c7f3
Mario <msanbar693a@ieslacampina.es>
2024-06-03 12:25:54 +0200

    Nuevo Index

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
-require 'conexion.php';
+session_start();
 
-$sql = "SELECT * FROM zapatillas";
-
-$resultado = $mysqli->query($sql);
+// Verifica si hay una sesión activa
+if (isset($_SESSION['usuario'])) {
+    $usuario = $_SESSION['usuario'];
+}
 ?>
 
 <!doctype html>
@@ -26,26 +27,35 @@ $resultado = $mysqli->query($sql);
 
 
 </head>
-<?php
-
-?>
 
 <body>
     <div class="jumbotron">
         <h1 class="display-3">Hola, bienvenido a Snkrs.Pro</h1>
-        <p class="lead">Esta es la página de administración de la base de datos de nuestras zapatillas.</p>
-        <hr class="my-2">
-        <p>Deberá registrarse o iniciar sesión para poder acceder</p>
-        <p class="lead">
-            <a class="btn btn-success btn-lg" href="registrarUsu.php" role="button">Registrarse</a>
-            <a class="btn btn-primary btn-lg" href="zapatillas.php" role="button">Iniciar Sesión</a>
-            <a class="btn btn-primary btn-lg" href="usuarios.php" role="button">Administrar Usuarios</a>
+        <?php if (isset($usuario)) { ?>
+            <p class="lead">Bienvenido, <?php echo $usuario; ?> !</p>
+        <?php } else { ?>
+            <p class="lead">Esta es la página de administración de la base de datos de nuestras zapatillas.</p>
+            <hr class="my-2">
+            <p class="lead">Deberá registrarse o iniciar sesión para poder acceder</p>
+            <p class="lead">
+                <a class="btn btn-success btn-lg" href="registrarUsu.php" role="button">Registrarse</a>
+            </p>
+            <p class="lead">
+                <a class="btn btn-primary btn-lg" href="zapatillas.php" role="button">Iniciar Sesión</a>
+                <a class="btn btn-primary btn-lg" href="usuarios.php" role="button">Administrar Usuarios</a>
+            </p>
+        <?php } ?>
 
-        </p>
-        <a class="btn btn-danger btn-lg" href="cerrar.php" role="button">Cerrar Sesión</a>
-
+        <?php if (isset($usuario)) { ?>
+            <p class="lead">
+                <a class="btn btn-primary btn-lg" href="zapatillas.php" role="button">Zapatillas</a>
+                <a class="btn btn-primary btn-lg" href="usuarios.php" role="button">Usuarios</a>
+            </p>
+            <p class="lead">
+                <a class="btn btn-danger btn-lg" href="cerrar.php" role="button">Cerrar Sesión</a>
+            </p>
+        <?php } ?>
     </div>
-
 
 </body>
 
