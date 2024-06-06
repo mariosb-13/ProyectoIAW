253b510fef7ec84c32279d72ff462ac5c14cfda8
Mario <msanbar693a@ieslacampina.es>
2024-06-06 08:43:45 +0200

    Comprobar admin en password

--- cambiarPassword.php
+++ cambiarPassword.php
@@ -1,8 +1,29 @@
 <?php
 require 'conexion.php';
 
+session_start();
+
+if (!isset($_SESSION['usuario'])) {
+    // Si el usuario no ha iniciado sesión, redirigirlo al formulario de inicio de sesión
+    header("Location: login.php");
+    exit();
+}
+
 $id = $_GET['id'];
 
+
+$usuario = $_SESSION['usuario'];
+
+// Obtener si el usuario es admin
+$sql = $mysqli->prepare("SELECT administrador FROM usuarios WHERE usuario = ?");
+$sql->bind_param("s", $usuario);
+$sql->execute();
+$resultado = $sql->get_result();
+$fila = $resultado->fetch_assoc();
+$admin = $fila['administrador'] == 'Si';
+
+$sql = "SELECT * FROM zapatillas";
+$resultado = $mysqli->query($sql);
 ?>
 
 <!doctype html>
@@ -23,33 +44,42 @@ $id = $_GET['id'];
 
     <title>Snkrs.Pro - Cambiar Contraseña</title>
 </head>
+<?php if ($admin) { ?>
 
-<body>
-    <div class="container">
-        <div class="row">
-            <h1>Cambiar Contraseña</h1>
-        </div>
+    <body>
+        <div class="container">
+            <div class="row">
+                <h1>Cambiar Contraseña</h1>
+            </div>
 
-        <div class="row">
-            <div class="col-md-8">
-                <form id="cambio_password" name="cambio_password" autocomplete="off" action="cambiarPassword2.php?id=<?php echo $id; ?>" method="post">
-                    <div class="form-group">
-                        <label for="nueva_password">Nueva Contraseña:</label>
-                        <input type="password" name="nueva_password" id="nueva_password" class="form-control" required>
-                    </div>
+            <div class="row">
+                <div class="col-md-8">
+                    <form id="cambio_password" name="cambio_password" autocomplete="off" action="cambiarPassword2.php?id=<?php echo $id; ?>" method="post">
+                        <div class="form-group">
+                            <label for="nueva_password">Nueva Contraseña:</label>
+                            <input type="password" name="nueva_password" id="nueva_password" class="form-control" required>
+                        </div>
 
-                    <div class="form-group">
-                        <label for="confirmar_password">Confirmar Contraseña:</label>
-                        <input type="password" name="confirmar_password" id="confirmar_password" class="form-control" required>
-                    </div>
+                        <div class="form-group">
+                            <label for="confirmar_password">Confirmar Contraseña:</label>
+                            <input type="password" name="confirmar_password" id="confirmar_password" class="form-control" required>
+                        </div>
 
-                    <div class="form-group">
-                        <input type="submit" class="btn btn-primary" value="Cambiar Contraseña">
-                    </div>
-                </form>
+                        <div class="form-group">
+                            <input type="submit" class="btn btn-primary" value="Cambiar Contraseña">
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
+    </body>
+
+<?php } else { ?>
+    <div class='container mt-4'>
+        <div class='alert alert-success text-center' role='alert'>
+            El usuario ha sido añadido correctamente <a href='index.php' class='alert-link'>Volver</a>.
+        </div>
     </div>
-</body>
+<?php } ?>
 
 </html>
